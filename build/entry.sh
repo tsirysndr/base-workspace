0e466db7817c8ce5f453b1da80e9de7081690f19
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-08-31 09:29:01 +0200

    fix: allow non root user to run docker commands

--- build/entry.sh
+++ build/entry.sh
@@ -2,4 +2,7 @@
 
 sudo dockerd-entrypoint.sh >/dev/null 2>/dev/null &
 
+sudo chown -R $(whoami) /var/run/docker.sock
+
 code -v tunnel
+
--- devenv/entry.sh
+++ devenv/entry.sh
@@ -2,4 +2,6 @@
 
 sudo dockerd-entrypoint.sh >/dev/null 2>/dev/null &
 
+sudo chown -R $(whoami) /var/run/docker.sock
+
 code -v tunnel
